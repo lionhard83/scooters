f5c2ba85c952290207c6d513e08bb75b7fe63db8
lionhard83 <carloleonardi83@gmail.com>
2023-01-09 13:29:49 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { scooters } from "./data/scooters";
+import { Scooter } from "./models/Scooter";
+
+const app = express();
+
+function getDistance(x1: number, y1: number, x2: number, y2: number) {
+  let y = x2 - x1;
+  let x = y2 - y1;
+  return Math.sqrt(x * x + y * y);
+}
+
+app.get("/scooters", (req, res) => {
+  let scts: (Scooter & { distance?: number })[] = [...scooters];
+  const { lat, long, maxDistance } = req.query;
+  if (lat && long && maxDistance) {
+    scts = scts.map((scooter) => {
+      return {
+        ...scooter,
+        distance: getDistance(
+          scooter.lat,
+          scooter.long,
+          Number(lat),
+          Number(long)
+        ),
+      };
+    });
+    const result = scts
+      .filter(({ distance }) => distance! < Number(maxDistance))
+      .sort((a, b) => (a.distance! > b.distance! ? 1 : -1));
+    return res.json(result);
+  }
+  res.json(scts);
+});
+
+app.listen(3000, () => console.log("Server is running"));
---
+++ src/models/Scooter.ts
@@ -0,0 +1,6 @@
+export type Scooter = {
+  id: string;
+  lat: number;
+  long: number;
+  busy: boolean;
+};
